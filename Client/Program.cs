b5fdc39585d6d6358a1583a21e8a7a26ac345321
TalonEzio <vuthemanh1707@gmail.com>
2023-12-18 18:11:01 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static int _number;
+        private static readonly Random Random = new();
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            Input();
+            Console.WriteLine("Bam phim bat ki de ket thuc chuong trinh");
+            Console.ReadLine();
+        }
+
+        static void Input()
+        {
+            while (_number <= 10000)
+            {
+                _number = Random.Next(11000);//thay gia tri lon nhat random vao day
+                Console.WriteLine($"Gia tri : {_number}");
+                SendToServer(_number);
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void SendToServer(int number)
+        {
+            try
+            {
+                var serverIpAddess = IPAddress.Loopback;
+                var serverPort = 5000;
+                var serverEndPoint = new IPEndPoint(serverIpAddess, serverPort);
+
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(serverEndPoint);
+                if (socket.Connected)
+                {
+                    var numberBytes = BitConverter.GetBytes(number);
+                    socket.Send(numberBytes);
+                    socket.Shutdown(SocketShutdown.Send);
+                    Console.WriteLine($"Da gui du lieu toi {serverEndPoint} thanh cong");
+                }
+                else
+                {
+                    Console.WriteLine("Khong the ket noi!!!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static int _number = 0;
+        private static readonly List<int> CacheClient = new List<int>();
+        private static bool _isStopped = false;
+        static void Main()
+        {
+
+            Console.Title = "Server";
+
+            var ipAddress = IPAddress.Loopback;
+            var port = 5000;
+            var endPoint = new IPEndPoint(ipAddress, port);
+
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(endPoint);
+            listener.Listen(10);
+
+            var thread = new Thread(() => HandleClient(listener));
+            thread.Start();
+
+
+            while (!_isStopped)
+            {
+                //Nothing...
+            }
+            thread.DisableComObjectEagerCleanup();
+            
+            Console.WriteLine("Bam phim bat ki de ket thuc chuong trinh");
+            Console.ReadKey();
+        }
+
+        static void HandleClient(Socket listener)
+        {
+            const int bufferSize = 1024;
+            var buffer = new byte[bufferSize];
+            Console.WriteLine("Server da san sang");
+            while (true)
+            {
+                var socket = listener.Accept();
+                Console.WriteLine($"Da nhan ket noi tu {socket.RemoteEndPoint}");
+                var receiveLength = socket.Receive(buffer);
+                socket.Shutdown(SocketShutdown.Receive);
+                _number = BitConverter.ToInt32(buffer, 0);
+                if (_number > 10000)
+                {
+                    _isStopped = true;
+                    break;
+                }
+
+                CacheClient.Add(_number);
+                Console.WriteLine($"Gia tri nho nhat phia client: {CacheClient.Min()}");
+                Array.Clear(buffer, 0, receiveLength);
+            }
+        }
+    }
+}
